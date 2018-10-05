871305b14d36ab3fc57f07b2089c74f1c639b9b1
mr Yacine <mryacine@macbook-air-de-mr.local>
2018-10-05 02:55:43 +0100

    Initial Commit

---
+++ quranTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  quranTest
+//
+//  Created by mr Yacine on 10/5/18.
+//  Copyright © 2018 mr Yacine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
